e45f31635c8a0f6ce641693b684483369305f657
Dilyana Zheleva <di.zheleva89@gmail.com>
2024-11-03 14:31:24 +0200

    updated content

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <>
+            <section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, blanditiis adipisci? Officiis ab quibusdam eum, magni unde quod dolor deleniti est dignissimos, dolorum, rerum aperiam excepturi tempora sapiente numquam porro!</p>
+            </section>
+        </>
+    );
+}
